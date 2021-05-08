9d6caee3fa31a2255910e1261b39a4081959d983
paarshan4800 <paargav.shuttle@gmail.com>
2021-05-08 20:05:21 +0530

    Migrated to postgres

--- api/__init__.py
+++ api/__init__.py
@@ -11,6 +11,8 @@ import threading
 
 from api.queue import Queue
 
+from decouple import config
+
 logging.basicConfig(filename="paint.log", level=logging.DEBUG, format="%(asctime)s : %(levelname)s : %(message)s")
 
 app = Flask(__name__)
@@ -19,20 +21,20 @@ CORS(app)
 
 mysql = MySQL(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:PaarShanDB0408@localhost:3306/ctfsd'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SECRET_KEY'] = 'ZA8cPQfHALBMYgUE4EdQUCCMj5XmA7889NikUDIztrNfGWmsYH1OwXznJBLmPMG'
-app.config['BUNDLE_ERRORS'] = True
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'paarshan4800@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ggmu1008'
-app.config['MAIL_DEFAULT_SENDER'] = 'paarshan4800@gmail.com'
-app.config['GOOGLE_CLIENT_ID'] = '994181888367-tblo1lfujqi80tc857vflslokmj416fj.apps.googleusercontent.com'
-app.config['GOOGLE_CLIENT_SECRET'] = 'LJRWQWFprqsyJtILtiSGdHgx'
-app.config['GOOGLE_DISCOVERY_URL'] = 'https://accounts.google.com/.well-known/openid-configuration'
-app.config['APP_BASE_URL'] = 'https://127.0.0.1:5000'
+app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config('SQLALCHEMY_TRACK_MODIFICATIONS')
+app.config['SECRET_KEY'] = config('SECRET_KEY')
+app.config['BUNDLE_ERRORS'] = config('BUNDLE_ERRORS')
+app.config['MAIL_SERVER'] = config('MAIL_SERVER')
+app.config['MAIL_PORT'] = config('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = config('MAIL_USE_TLS')
+app.config['MAIL_USERNAME'] = config('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = config('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = config('MAIL_DEFAULT_SENDER')
+app.config['GOOGLE_CLIENT_ID'] = config('GOOGLE_CLIENT_ID')
+app.config['GOOGLE_CLIENT_SECRET'] = config('GOOGLE_CLIENT_SECRET')
+app.config['GOOGLE_DISCOVERY_URL'] = config('GOOGLE_DISCOVERY_URL')
+app.config['APP_BASE_URL'] = config('APP_BASE_URL')
 
 mail = Mail(app)
 
