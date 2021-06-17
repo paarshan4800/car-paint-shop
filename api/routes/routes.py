cecfb2d56b8d733b1249251a226560d1561cbca8
Paargav Shanker <paargav.shuttle@gmail.com>
2021-06-17 18:17:45 +0530

    updated welcome message

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -9,7 +9,10 @@ from flask import redirect
 @app.route("/", methods=["GET"])
 def welcome():
     try:
-        return {"message": "Hello mate. This is from CTF Student Directors Recruitment Task API. Cheers!"}, 200
+        return {
+                   "message": "Hello mate. Welcome to Car Paint Shop API. You can find the API docs at the below link. Cheers!",
+                   "link": "https://car-paint-shop.herokuapp.com/api-docs"
+               }, 200
 
     except Exception as e:
         logging.error("Error in welcome route - {}".format(e))
